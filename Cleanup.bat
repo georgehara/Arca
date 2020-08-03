e6d17f9316a7342053df43c89ba88fb3ddb35e2d
georgehara <24386479+georgehara@users.noreply.github.com>
2020-08-03 19:21:55 +0300

    Update Cleanup.bat

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,30 +1,36 @@
-REM Not working yet.
-
 echo off
 
+REM Change the path to match your environment
 cd "C:\Stuff\Program data\VS\GitHub\Arca\"
 
-start for /f "delims=," %i in ('dir /a:d /s /b bin') do rd /s /q "%i"
+echo Deleting the 'bin' folders...
+for /f "delims=," %%i in ('dir /a:d /s /b bin') do rd /s /q "%%i"
 
 if %ERRORLEVEL% NEQ 0 (
-	echo Failed to delete 'bin' folders.
+	echo Failed to delete the 'bin' folders.
 	goto End
 )
 
 
-start for /f "delims=," %i in ('dir /a:d /s /b obj') do rd /s /q "%i"
+echo Deleting the 'obj' folders...
+for /f "delims=," %%i in ('dir /a:d /s /b obj') do rd /s /q "%%i"
 
 if %ERRORLEVEL% NEQ 0 (
-	echo Failed to delete 'obj' folders.
+	echo Failed to delete the 'obj' folders.
 	goto End
 )
 
+
+echo Deleting the NuGet packages...
 del /q OutputPackages\*.*
 
 if %ERRORLEVEL% NEQ 0 (
 	echo Failed to delete the NuGet packages.
 	goto End
 )
+
+
+echo Finished.
 
 :End
 
