a895fa84e3d100eb170f2cd1b26383a027cdca1c
georgehara <24386479+georgehara@users.noreply.github.com>
2025-11-24 17:43:28 +0200

    Fix Publish script

--- Management/PublishPackagesToGitHubAndNuget.bat
+++ Management/PublishPackagesToGitHubAndNuget.bat
@@ -15,67 +15,67 @@ echo Publishing NuGet packages to GitHub
 REM dotnet nuget add source --username georgehara --password %GitHubApiKey% --store-password-in-clear-text --name github "%SourceUrl%"
 
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.Core.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.Core.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Attributes.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Attributes.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Attributes.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Attributes.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Extensions.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Extensions.DependencyInjection.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Extensions.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Extensions.Specialized.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Implementations.ForMicrosoft.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Implementations.ForMicrosoft.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Libraries.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Libraries.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Manager.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Manager.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Single.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Single.%PackageVersion%.nupkg" --source %GitHubSource% --api-key %GitHubApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
@@ -86,67 +86,67 @@ echo The NuGet packages were successfully published to GitHub.
 
 echo Publishing NuGet packages to NuGet
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.Core.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.Core.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Abstractions.Specialized.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Abstractions.Specialized.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Attributes.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Attributes.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Attributes.Specialized.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Attributes.Specialized.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Extensions.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Extensions.DependencyInjection.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Extensions.Specialized.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Extensions.Specialized.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Implementations.ForMicrosoft.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Implementations.ForMicrosoft.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Libraries.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Libraries.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Manager.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Manager.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
 )
 
-dotnet nuget push "../OutputPackages/Automated.Arca.Single.%PackageVersion%.nupkg" --source %NugetSourceUrl% --api-key %NugetApiKey%
+dotnet nuget push "../Arca/OutputPackages/Automated.Arca.Single.%PackageVersion%.nupkg" --source %NugetSource% --api-key %NugetApiKey% --skip-duplicate
 if %ERRORLEVEL% NEQ 0 (
 	echo The NuGet package was not published.
 	goto End
